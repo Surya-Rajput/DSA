2b6da45e72533f263e565d025f45ab192b136b1f
Surya-Rajput <rajputsurya5678@gmail.com>
2025-01-20 16:52:29 -0500

    two sum sorting

--- TwoSum.py
+++ TwoSum.py
@@ -24,3 +24,30 @@ class Solution:
     
 solution = Solution()
 print(solution.twosum([3,4,5,6], 7))
+
+# Time Complexity : O(n^2)
+# Space complexity: O(1)
+
+
+# Sorting method
+
+class Solution2:
+    def twosum(self, nums : list[int], target : int) -> list[int]:
+        A = []
+        for i, num in enumerate(nums):
+            A.append([num, i])
+        A.sort()
+        i, j = 0, len(nums) -1
+        while i<j:
+            cur = A[i][0] + A[j][0]
+            if cur == target:
+                return [min(A[i][1],A[j][1]), max(A[i][1], A[j][1])]
+            elif cur < target:
+                i += 1
+            else:
+                j -= 1
+        return []
+
+
+
+
